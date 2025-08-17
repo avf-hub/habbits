fb0a2df5aa1dcec4dd6b74eae515ff285cca0e1d
Andrey Falkovskiy <avf-91@yandex.ru>
2025-08-17 16:13:54 +0300

    Появление попап окна

--- scripts/app.js
+++ scripts/app.js
@@ -16,6 +16,9 @@ const page = {
 	content: {
 		daysContainer: document.getElementById("days"),
 		nextDay: document.querySelector(".habbit__day")
+	},
+	popup: {
+		index: document.getElementById("add-habbit-popup")
 	}
 };
 
@@ -32,6 +35,14 @@ function loadData() {
 
 function saveData() {
 	localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+function togglePopup() {
+	if (page.popup.index.classList.contains("cover_hidden")) {
+		page.popup.index.classList.remove("cover_hidden");
+	} else {
+		page.popup.index.classList.add("cover_hidden");
+	}
 }
 
 /* render */
@@ -99,6 +110,7 @@ function rerender(activeHabbitId) {
 }
 
 /* work from days */
+
 function addDays(event) {
 	const form = event.target;
 	event.preventDefault();
